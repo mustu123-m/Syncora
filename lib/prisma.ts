c879cb8eae4effd5eb3ce54513f436c5c1dc7c9e
mustu123-m <xgreenmustafaattarwala10@gmail.com>
2026-03-25 16:04:33 +0530

    fixing prismaschema1

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,15 @@
 import 'dotenv/config'
 import { PrismaClient } from '@prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
+
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! })
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
-
+0
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
-  })
+  new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
