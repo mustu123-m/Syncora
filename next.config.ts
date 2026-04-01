b580c52556d7136b4fadcb6f6a05e6275c4d9365
mustu123-m <xgreenmustafaattarwala10@gmail.com>
2026-04-02 00:41:23 +0530

    fixing production issues

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ["lh3.googleusercontent.com"]  // allow Google profile pictures
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com"
+      }
+    ]
   }
-};
+}
 
-export default nextConfig;
+export default nextConfig
